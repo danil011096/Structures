d4cfeab15410a1aec8a744458aec88adc266bb50
E1Bernardo <goonypony@gmail.com>
2023-10-03 21:17:59 +0300

    ничего не поменял

--- Structures/Main.cpp
+++ Structures/Main.cpp
@@ -1,46 +1,46 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <string>
 
 
-struct Date { // Создание значений по умолчанию
+struct Date { // РЎРѕР·РґР°РЅРёРµ Р·РЅР°С‡РµРЅРёР№ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
 	int day = 01;
 	int month = 01;
 	int year = 1970;
 	std::string note = "empty";
 };
-void print_date(const Date& D) { //создание функции для вывода оъектов целиком
+void print_date(const Date& D) { //СЃРѕР·РґР°РЅРёРµ С„СѓРЅРєС†РёРё РґР»СЏ РІС‹РІРѕРґР° РѕСЉРµРєС‚РѕРІ С†РµР»РёРєРѕРј
 	std::cout << D.day << '.' << D.month << '.' << D.year << " - " << D.note << std::endl;
 }
 
 Date input_date() {
 	Date tmp;
 
-	std::cout << "Введите день -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РґРµРЅСЊ -> ";
 	std::cin >> tmp.day;
-	std::cout << "Введите месяц -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РјРµСЃСЏС† -> ";
 	std::cin >> tmp.month;
-	std::cout << "Введите год -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РіРѕРґ -> ";
 	std::cin >> tmp.year;
 	std::cin.ignore();
-	std::cout << "Введите запись -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ Р·Р°РїРёСЃСЊ -> ";
 	std::getline(std::cin, tmp.note);
 
 	return tmp;
 }
 
 void fill_date(Date& D) {
-	std::cout << "Введите день -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РґРµРЅСЊ -> ";
 	std::cin >> D.day;
-	std::cout << "Введите месяц -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РјРµСЃСЏС† -> ";
 	std::cin >> D.month;
-	std::cout << "Введите год -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ РіРѕРґ -> ";
 	std::cin >> D.year;
 	std::cin.ignore();
-	std::cout << "Введите запись -> ";
+	std::cout << "Р’РІРµРґРёС‚Рµ Р·Р°РїРёСЃСЊ -> ";
 	std::getline(std::cin, D.note);
 }
 
-//Задача 1.
+//Р—Р°РґР°С‡Р° 1.
 struct MyDate {
 	int day = 1;
 	int month = 1;
@@ -53,9 +53,9 @@ struct CoinKeeper {
 	int* coins = nullptr;
 };
 void print_keeper(const CoinKeeper& CK) {
-	std::cout << "Имя: " << CK.name << std::endl;
-	std::cout << "Дата рождения: " << CK.birthday.day << '.' << CK.birthday.month << '.' << CK.birthday.year << std::endl;
-	std::cout << "Монеты: ";
+	std::cout << "РРјСЏ: " << CK.name << std::endl;
+	std::cout << "Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ: " << CK.birthday.day << '.' << CK.birthday.month << '.' << CK.birthday.year << std::endl;
+	std::cout << "РњРѕРЅРµС‚С‹: ";
 	for (int i = 0; i < CK.coins_number; i++)
 		std::cout << CK.coins[i] << ' ';
 	std::cout << std::endl;
@@ -71,7 +71,7 @@ int cash(const CoinKeeper& CK) {
 	return sum;
 }
 
-//Задача 2.
+//Р—Р°РґР°С‡Р° 2.
 struct Point {
 	double X{};
 	double Y{};
@@ -90,61 +90,61 @@ int main () {
 	setlocale(LC_ALL, "Russian");
 		int n;
 
-		//Структуры: создание, мнговенная инициализация, работа с полями
-		struct Person { // Создание структуры, название структуры с большой буквы
+		//РЎС‚СЂСѓРєС‚СѓСЂС‹: СЃРѕР·РґР°РЅРёРµ, РјРЅРіРѕРІРµРЅРЅР°СЏ РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ, СЂР°Р±РѕС‚Р° СЃ РїРѕР»СЏРјРё
+		struct Person { // РЎРѕР·РґР°РЅРёРµ СЃС‚СЂСѓРєС‚СѓСЂС‹, РЅР°Р·РІР°РЅРёРµ СЃС‚СЂСѓРєС‚СѓСЂС‹ СЃ Р±РѕР»СЊС€РѕР№ Р±СѓРєРІС‹
 			std::string name;
 			int age;
 			std::string job;
 			double salary;
 		} p3, p4{ "Unlnow person", 18 };
 
-		Person p1; // Создание объекта на основе структуры Person
-		// Инициализация полей объекта p1
+		Person p1; // РЎРѕР·РґР°РЅРёРµ РѕР±СЉРµРєС‚Р° РЅР° РѕСЃРЅРѕРІРµ СЃС‚СЂСѓРєС‚СѓСЂС‹ Person
+		// РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРѕР»РµР№ РѕР±СЉРµРєС‚Р° p1
 		p1.name = "Tom Smith"; 
 		p1.age = 27;
 		p1.job = "programer";
 		p1.salary = 80000;
-		// Поля объекта - элементы структуры
-		// Вывод полей объекта p1
-		std::cout << "Имя: " << p1.name << std::endl;
-		std::cout << "Возраст: " << p1.age << std::endl;
-		std::cout << "Должность: " << p1.job << std::endl;
-		std::cout << "Зарплата: " << p1.salary << std::endl;
+		// РџРѕР»СЏ РѕР±СЉРµРєС‚Р° - СЌР»РµРјРµРЅС‚С‹ СЃС‚СЂСѓРєС‚СѓСЂС‹
+		// Р’С‹РІРѕРґ РїРѕР»РµР№ РѕР±СЉРµРєС‚Р° p1
+		std::cout << "РРјСЏ: " << p1.name << std::endl;
+		std::cout << "Р’РѕР·СЂР°СЃС‚: " << p1.age << std::endl;
+		std::cout << "Р”РѕР»Р¶РЅРѕСЃС‚СЊ: " << p1.job << std::endl;
+		std::cout << "Р—Р°СЂРїР»Р°С‚Р°: " << p1.salary << std::endl;
 
 		std::cout << "_______________________________________" << std::endl << std::endl;
 
-		Person p2{ // Инициализация полей объекта p2
+		Person p2{ // РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРѕР»РµР№ РѕР±СЉРµРєС‚Р° p2
 			"Bob Thomas",
 			32,
 			"director",
 			120000
 		};
-		// Вывод полей объекта p2
-		std::cout << "Имя: " << p2.name << std::endl;
-		std::cout << "Возраст: " << p2.age << std::endl;
-		std::cout << "Должность: " << p2.job << std::endl;
-		std::cout << "Зарплата: " << p2.salary << std::endl;
+		// Р’С‹РІРѕРґ РїРѕР»РµР№ РѕР±СЉРµРєС‚Р° p2
+		std::cout << "РРјСЏ: " << p2.name << std::endl;
+		std::cout << "Р’РѕР·СЂР°СЃС‚: " << p2.age << std::endl;
+		std::cout << "Р”РѕР»Р¶РЅРѕСЃС‚СЊ: " << p2.job << std::endl;
+		std::cout << "Р—Р°СЂРїР»Р°С‚Р°: " << p2.salary << std::endl;
 
 
-		p3 = p1; // Копирование значение полей объекта p1 в поля объекта p3
+		p3 = p1; // РљРѕРїРёСЂРѕРІР°РЅРёРµ Р·РЅР°С‡РµРЅРёРµ РїРѕР»РµР№ РѕР±СЉРµРєС‚Р° p1 РІ РїРѕР»СЏ РѕР±СЉРµРєС‚Р° p3
 
-		//Указатели и функции при работе со структурами
+		//РЈРєР°Р·Р°С‚РµР»Рё Рё С„СѓРЅРєС†РёРё РїСЂРё СЂР°Р±РѕС‚Рµ СЃРѕ СЃС‚СЂСѓРєС‚СѓСЂР°РјРё
 		Date d1;
-		print_date(d1); //передача объектов в функцию
+		print_date(d1); //РїРµСЂРµРґР°С‡Р° РѕР±СЉРµРєС‚РѕРІ РІ С„СѓРЅРєС†РёСЋ
 
 
-		Date* pd1 = &d1; // Создание указателя области
+		Date* pd1 = &d1; // РЎРѕР·РґР°РЅРёРµ СѓРєР°Р·Р°С‚РµР»СЏ РѕР±Р»Р°СЃС‚Рё
 	
 		std::cout << std::endl;
-		std::cout << "Ввод данных в объект\n\n";
-	//	Date d2 = input_date(); //запись функции в объект d2
+		std::cout << "Р’РІРѕРґ РґР°РЅРЅС‹С… РІ РѕР±СЉРµРєС‚\n\n";
+	//	Date d2 = input_date(); //Р·Р°РїРёСЃСЊ С„СѓРЅРєС†РёРё РІ РѕР±СЉРµРєС‚ d2
 	//	print_date(d2);
 		Date d2;
-		fill_date(d2); //запись функции в объект d2
+		fill_date(d2); //Р·Р°РїРёСЃСЊ С„СѓРЅРєС†РёРё РІ РѕР±СЉРµРєС‚ d2
 		print_date(d2);
 
-		//Задача 1. Хранитель монет 
-		std::cout << "Задача 1.\n";
+		//Р—Р°РґР°С‡Р° 1. РҐСЂР°РЅРёС‚РµР»СЊ РјРѕРЅРµС‚ 
+		std::cout << "Р—Р°РґР°С‡Р° 1.\n";
 		CoinKeeper ck{
 			"Robert Poson",
 			{ 11, 12, 2003 },
@@ -152,12 +152,12 @@ int main () {
 			new int [5] { 2, 10, 4, 5, 1}
 		};
 		print_keeper(ck);
-		std::cout << "Возраст: " << age(ck, 2023) << std::endl;
-		std::cout << "Сумма монет = " << cash(ck) << std::endl;
+		std::cout << "Р’РѕР·СЂР°СЃС‚: " << age(ck, 2023) << std::endl;
+		std::cout << "РЎСѓРјРјР° РјРѕРЅРµС‚ = " << cash(ck) << std::endl;
 		delete[] ck.coins;
 		
-		//Задача 2. Точки в плоскости
-		std::cout << "Задача 2.\n";
+		//Р—Р°РґР°С‡Р° 2. РўРѕС‡РєРё РІ РїР»РѕСЃРєРѕСЃС‚Рё
+		std::cout << "Р—Р°РґР°С‡Р° 2.\n";
 		Point A{ 2.2, 5.0 }, B{ 7.1, 6.8 };
 		std::cout << compare_distance(A, B) << std::endl;
 		std::cout << compare_distance({5.5, 1.1}, {3.3, 0.77}) << std::endl;
